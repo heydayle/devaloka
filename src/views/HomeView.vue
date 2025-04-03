2e2527519175a17f3ea7bd2dea12acfdd313a53f
heyday <hungthinh.ckc@gmail.com>
2025-04-03 23:16:54 +0700

    update credit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,11 +6,27 @@ import Cube3D from '@/components/Cube3D.vue';
 <template>
   <main style="text-align: center;">
     <Cube3D style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: -1;" />
-    <p style="font-size: 200px; margin-bottom: -20px;">Devaloka</p>
-    <p style="font-size: 24px; font-family: 'Shavina Elegance'; margin-bottom: 0;">
+    <p style="font-size: 200px; margin-bottom: -50px;">Devaloka</p>
+    <p class="content">
       Infinite realm of creation, where
       language and the
       cosmos resonate.
     </p>
+    <div style="margin-top: 32px;">
+      <p class="content">
+        <a href="https://thinh.io.vn" target="_blank"
+          style="color: goldenrod; border-bottom: 1px solid goldenrod;padding-bottom: 0; padding: 0 12px 0px;">
+          @Thinh Le
+        </a>
+      </p>
+      <p class="content">2025</p>
+    </div>
   </main>
 </template>
+<style>
+.content {
+  font-size: 24px;
+  font-family: 'Shavina Elegance';
+  margin-bottom: 0;
+}
+</style>
